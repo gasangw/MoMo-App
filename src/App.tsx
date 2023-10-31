325e3bf31d71d569038f8347ab4a991813076037
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-10-31 19:05:21 +0200

    style: configured fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl text-red-600 font-bold'>Gasangwa</h1>
+    <div className='bg-blue-900'>
+      <h1 className='text-6xl text-yellow-500 font-Poppins font-extrabold'>Gasangwa</h1>
+      <p className='text-3xl font-Assistant font-light'>
+        Lorem ipsum, debitis eveniet nam incidunt sequi?
+      </p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+        Assistant: ['Assistant', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          900: '#004f71',
+        },
+        gray: {
+          100: '#F9FAFB',
+          600: '#81849c',
+        },
+        yellow: {
+          500: '#ffcb05',
+        },
+      },
+    },
   },
   plugins: [],
 }
