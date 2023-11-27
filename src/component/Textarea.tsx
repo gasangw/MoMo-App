1f299ae11ec06952d6fbf9625c8335c486077dad
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-11-28 01:07:17 +0200

    style: edit the placeholder in the textarea

--- src/component/Textarea.tsx
+++ src/component/Textarea.tsx
@@ -2,8 +2,8 @@ export default function Textarea() {
   return (
     <div>
       <textarea
-        className='h-40 w-full rounded-2xl border-2 border-gray-200 p-2 focus:border-blue-900 focus:outline-none'
-        placeholder='Type your message here...'
+        className='h-40 w-full rounded-2xl border-2 border-gray-200 p-2 focus:outline-none'
+        placeholder='Type your message here... i.e what you deal in or what you will be selling/ services you will be providing.'
       ></textarea>
     </div>
   )
