4cb1697eb1ce7b5b94c577c52e0cbddaacdd55a8
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-01 00:42:00 +0200

    feat: create a new feature of a downloadIcon in the assests

--- src/pages/KYCDocuments.tsx
+++ src/pages/KYCDocuments.tsx
@@ -1,3 +1,20 @@
+import applicationForm from '../assets/GitNotesForProfessionals.pdf'
+import StyleWrapper from '../component/StyleWrapper'
+
 export default function KYCDocuments() {
-  return <div>KYCDocuments</div>
+  return (
+    <StyleWrapper className='pt-8 text-blue-900'>
+      <h2 className='py-5 font-Poppins text-3xl font-bold'>KYC Contracting and File Management</h2>
+
+      <h4 className='text-lg font-medium'>Download Contracting Requirements</h4>
+      <p className='py-3 text-sm italic'>
+        Fill and sign these files and then upload them again in the field below
+        <div className='mt-3 bg-gray-200 px-2 py-3'>
+          <a href={applicationForm} download>
+            Download Application Form
+          </a>
+        </div>
+      </p>
+    </StyleWrapper>
+  )
 }
