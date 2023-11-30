4e16399455054f8617aee4d1361c00bad957764b
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-01 01:32:04 +0200

    style: implement the styles for the texts, inputs and titles in the KYCDocument component

--- src/pages/KYCDocuments.tsx
+++ src/pages/KYCDocuments.tsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom'
 import DownloadIcon from '../assets/DownloadIcon'
 import applicationForm from '../assets/GitNotesForProfessionals.pdf'
+import Button from '../component/Button'
 import StyleWrapper from '../component/StyleWrapper'
+import UploadDocument from './UploadDocument'
 
 export default function KYCDocuments() {
   return (
@@ -11,7 +14,7 @@ export default function KYCDocuments() {
         <p className='py-3 font-Assistant text-sm italic'>
           Fill and sign these files and then upload them again in the field below
         </p>
-        <div className='mt-3 flex w-fit space-x-3 bg-gray-200 px-6 py-3'>
+        <div className='mt-3 flex w-full space-x-3 bg-gray-200 px-6 py-3'>
           <DownloadIcon />
           <a href={applicationForm} download className='text-base font-medium'>
             Download Application Forms
@@ -24,7 +27,25 @@ export default function KYCDocuments() {
         <p className='py-3 font-Assistant text-sm italic'>
           Upload all documents as a single PDF file
         </p>
+        <UploadDocument />
       </div>
+      <div className='mt-8 flex gap-4'>
+        <input type='checkbox' className='h-4 w-4' />
+        <p className='font-Assistant text-sm font-semibold'>
+          I/We certify that the information and supporting documentation provided in this
+          application is true and accurate, and that I/We commit to delivering the physical KYC
+          documents to MTN Rwanda within 7 days after submission. Failure to comply will lead to the
+          service being suspended. I/We also accept to be bound by the MTN Open API{' '}
+          <Link to='#' className='text-yellow-500'>
+            Terms and Conditions
+          </Link>
+          .
+        </p>
+      </div>
+      <Button
+        text='Submit'
+        className='mx-auto my-5 cursor-pointer bg-yellow-500 font-Assistant text-xl text-gray-100'
+      />
     </StyleWrapper>
   )
 }
--- src/pages/UploadDocument.tsx
+++ src/pages/UploadDocument.tsx
@@ -1,19 +1,19 @@
 import { ReactElement } from 'react'
+import DropZoneIcon from '../assets/DropZoneIcon'
 
 export default function UploadDocument(): ReactElement {
   return (
-    <div className='flex w-full items-center justify-center'>
+    <div className='flex w-full items-center justify-center md:items-start md:justify-start'>
       <label
         htmlFor='dropzone-file'
-        className='dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600'
+        className='dark:hover:bg-bray-800 flex h-36 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600'
       >
         <div className='flex flex-col items-center justify-center pb-6 pt-5'>
+          <DropZoneIcon />
           <p className='mb-2 text-sm text-gray-500 dark:text-gray-400'>
             <span className='font-semibold'>Click to upload</span> or drag and drop
           </p>
-          <p className='text-xs text-gray-500 dark:text-gray-400'>
-            SVG, PNG, JPG or GIF (MAX. 800x400px)
-          </p>
+          <p className='text-xs text-gray-500 dark:text-gray-400'>PDF (MAX. 800x400px or 1KB)</p>
         </div>
         <input id='dropzone-file' type='file' className='hidden' />
       </label>
