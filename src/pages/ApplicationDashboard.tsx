a50f3ae797fe51ce1d4e19496f17dfd09d7abee5
Thomas Gasangwa <thomasgasangwa@gmail.com>
2023-12-13 23:22:42 +0200

    feat: add assests and modify the existing ones

--- src/pages/ApplicationDashboard.tsx
+++ src/pages/ApplicationDashboard.tsx
@@ -1,7 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
+import BusinessIcon from '../assets/BusinessIcon'
+import ContractIcon from '../assets/ContractIcon'
+import KycIcon from '../assets/KycIcon'
 import Logo from '../assets/Logo'
 import LogoutIcon from '../assets/LogoutIcon'
-import SidebarProfileIcon from '../assets/SidebarProfileIcon'
+import ProductIcon from '../assets/ProductIcon'
+import UserProfile from '../assets/UserProfile'
 import MobileNavBar from './MobileNavBar'
 
 export default function ApplicationDashboard() {
@@ -16,7 +20,7 @@ export default function ApplicationDashboard() {
           <ul className='space-y-2 font-Assistant font-semibold'>
             <li className='text-gray-100 hover:text-gray-950'>
               <Link to='.' className='group flex items-center rounded-lg p-2 hover:bg-gray-100'>
-                <SidebarProfileIcon />
+                <UserProfile />
                 <span className='ms-3'>Profile</span>
               </Link>
             </li>
@@ -25,14 +29,7 @@ export default function ApplicationDashboard() {
                 to='business'
                 className='group flex items-center rounded-lg p-2 hover:bg-gray-100'
               >
-                <svg
-                  className='h-5 w-5 flex-shrink-0 text-yellow-500'
-                  aria-hidden='true'
-                  fill='currentColor'
-                  viewBox='0 0 18 18'
-                >
-                  <path d='M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z' />
-                </svg>
+                <BusinessIcon />
                 <span className='ms-3'>About Business</span>
               </Link>
             </li>
@@ -41,7 +38,7 @@ export default function ApplicationDashboard() {
                 to='products'
                 className='group flex items-center rounded-lg p-2 hover:bg-gray-100'
               >
-                <SidebarProfileIcon />
+                <ProductIcon />
                 <span className='ms-3'>Product</span>
               </Link>
             </li>
@@ -50,7 +47,7 @@ export default function ApplicationDashboard() {
                 to='contractRequirements'
                 className='group flex items-center rounded-lg p-2 hover:bg-gray-100'
               >
-                <SidebarProfileIcon />
+                <ContractIcon />
                 <span className='ms-3'>Contract Requirements</span>
               </Link>
             </li>
@@ -59,13 +56,13 @@ export default function ApplicationDashboard() {
                 to='kycdocuments'
                 className='group flex items-center rounded-lg p-2 hover:bg-gray-100'
               >
-                <LogoutIcon className='text-yellow-500' />
+                <KycIcon />
                 <span className='ms-3'>KYC Documents</span>
               </Link>
             </li>
             <li className='mt-auto grid text-gray-100 hover:text-gray-950'>
               <Link to='/' className='group flex items-center rounded-lg p-2 hover:bg-gray-100'>
-                <LogoutIcon className='text-yellow-500' />
+                <LogoutIcon />
                 <span className='ms-3'>Log Out</span>
               </Link>
             </li>
